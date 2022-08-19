59926f22b9b3ba9567e12c3024674396bf48d90e
Hayk <haykgevorgyan201@gmail.com>
2022-08-19 11:24:18 +0400

    adding swapping function, using jquery

--- grid.js
+++ grid.js
@@ -1,25 +1,21 @@
-const allSquares = document.querySelectorAll(".col");
-const container = document.querySelector(".container");
+const $lis = $('li');
+const $allSquares = $('.col');
 
+$(document).ready(function () {
+    $('.container .col').on('click', function () {
+        const clickedElement = $(this);
+        swappingNode(clickedElement);
+    });
 
-container.addEventListener("click", (event) => {
-    // .classList.contains(className);
-    const isClickedElementsquare = event.target.parentNode.classList.contains("row")
-    if (isClickedElementsquare && event.target.nextElementSibling) {
-        swappingNode(event.target, event.target.nextElementSibling);
-        event.target.style.backgroundColor = "green"
-        event.target.nextElementSibling.style.backgroundColor = "rgb(150, 71, 11)"
-    }else if (isClickedElementsquare && !event.target.nextElementSibling) {
-        swappingNode(event.target, event.target.parentNode.firstChild)
-        event.target.style.backgroundColor = "green"
-        event.target.parentNode.firstChild.style.backgroundColor = "rgb(150, 71, 11)"
+    function swappingNode(clickedElement) {
+        if (!clickedElement.is(':last-child')) {
+            clickedElement.insertAfter(clickedElement.next());
+        } else  {
+            const first = clickedElement.siblings().first();
+            clickedElement.insertBefore(first);
+            first.insertAfter(clickedElement.siblings().last());
+        }
     }
 });
 
-function swappingNode(node1, node2) {
-    const thirdElement = node2.nextElementSibling;
-    const leftNode = node1;
-    node1.replaceWith(node2)
-    node2.parentNode.insertBefore(leftNode, thirdElement)
-    // insertBefore(newNode, referenceNode);
-}
+
